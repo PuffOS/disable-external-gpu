d9d6b9495878b75da430f68a4a8a8f9ed50c3ddc
Ali Riza KESKIN <aliriza.keskin@pardus.org.tr>
2022-02-11 14:59:07 +0000

    gui improvement

--- gui/main.py
+++ gui/main.py
@@ -6,23 +6,38 @@ builder.add_from_file(os.path.dirname(os.path.abspath(__file__)) + "/main.ui")
 enabled = not os.path.exists("/var/cache/disable-gpu")
 
 def button_event(widget):
+    global enabled
+    i = 1
     if not enabled:
-        os.system("pkexec bash -c 'rm -f /var/cache/disable-gpu && reboot'")
+        i = os.system("pkexec bash -c 'rm -f /var/cache/disable-gpu'")
     else:
-        os.system("pkexec bash -c 'touch /var/cache/disable-gpu && reboot'")
+        i = os.system("pkexec bash -c 'touch /var/cache/disable-gpu'")
+    if i == 0:
+        enabled = not enabled
+        update_ui()
+        reboot.show()
+
+def reboot_event(widget):
+    os.system("pkexec reboot")
 
 button = builder.get_object("button")
 button.connect("clicked",button_event)
 
-if enabled:
-    button.set_label("Disable External GPU")
-else:
-    button.set_label("Enable External GPU")
+reboot = builder.get_object("reboot")
+reboot.connect("clicked",reboot_event)
+
+def update_ui():
+    if enabled:
+        button.set_label("Disable External GPU")
+    else:
+        button.set_label("Enable External GPU")
 
 
 window = builder.get_object("window")
 window.connect("destroy",Gtk.main_quit)
 window.show_all()
+update_ui()
+reboot.hide()
 
 Gtk.main()
 
